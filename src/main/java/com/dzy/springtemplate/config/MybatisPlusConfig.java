1e54235ffdb6f0c2998c4014addfefec27ba92e7
zhuzhiwei <zhuzhiwei@delonixmail.com>
2025-10-27 17:42:19 +0800

    feat 新增文件MybatisPlusConfig

---
+++ src/main/java/com/dzy/springtemplate/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.dzy.springtemplate.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * TODO
+ *
+ * @author zhuzhiwei
+ * @date 2025/10/27 17:10
+ */
+@Configuration
+@MapperScan("com.dzy.springtemplate")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+}
