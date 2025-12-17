8b53afaa559de7ee661ea90e3fde551113017093
zhuzhiwei <zhuzhiwei@delonixmail.com>
2025-12-17 15:52:20 +0800

    feat 代码优化

--- src/main/java/com/dzy/springtemplate/common/Result.java
+++ src/main/java/com/dzy/springtemplate/common/Result.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import org.slf4j.MDC;
 
 import java.io.Serializable;
 
